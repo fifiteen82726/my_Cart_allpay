47af7fe32e21758e59def580a4e11323ad26797b
fifiteen82726 <fifiteen82726@gmail.com>
2015-10-30 23:35:37 +0800

    rails g controller products index show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'carts/show'
+
+  get 'products/index'
+
+  get 'products/show'
+
   root 'products#index'
   resources :products, only: :show
   resource :cart, only: :show
