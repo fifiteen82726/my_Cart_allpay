5967a9b8ab5ce60b82f9b52b889559e7c720e82c
fifiteen82726 <fifiteen82726@gmail.com>
2015-11-04 01:40:20 +0800

    page 42完成,修改product#show view 新增加入購物車的按鈕,

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,10 @@ class ProductsController < ApplicationController
   end
 
   def show
-  	id = params[:id] 
-  	@product = Product.find(params[id])
+  	@product = Product.find(params[:id])
+
+  	#看不懂這句的意思是？
+  	@line_item = current_cart.line_items.find_or_initialize_by(product: @product)
+
   end
 end
