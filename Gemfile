a49cc9693f12c39add9265c04dbf3e39700bdcfd
fifiteen82726 <fifiteen82726@gmail.com>
2015-11-04 02:01:57 +0800

    add bootstrap

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '4.2.3'
 # Use postgresql as the database for Active Record
 gem 'pg'
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 5.0'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
@@ -24,6 +24,9 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'pry-rails', :group => :development
+
+gem 'bootstrap-sass', '~> 3.3.5'
+gem 'sass-rails', '>= 3.2'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,16 +1,17 @@
 class LineItemsController < ApplicationController
   def create
-  	@line_item = current_cart.line_item.new  line_item_params
+
+  	@line_item = current_cart.line_items.new  line_item_params
   	 if @line_item.save
-		redirect_to cart_path, notice: '􏰂􏰃􏰀􏰁Successfully create' 
-	 else
+		  redirect_to cart_path, notice: '􏰂􏰃􏰀􏰁Successfully create' 
+	   else
 	 	# 為何這樣 不知道
-	 	@product = @line_item.product
+	 	 @product = @line_item.product
       	render 'products/show'
-    end
+     end
 	 end
 
-  end
+
 
   def update
   end
