fd9d5539bd318e791ecf77de2f7eaad306e1d0aa
fifiteen82726 <fifiteen82726@gmail.com>
2015-11-04 14:09:53 +0800

    在購物車中可以袖出 update line_items的按鈕 但是按了沒反應

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,5 +2,24 @@ class CartsController < ApplicationController
   def show
   	@cart = Cart.find(current_cart)
   	@line_items = @cart.line_items.all
+
+  	
   end
+
+  def update
+	if current_cart.update cart_params redirect_to cart_path, notice: '􏰃􏰂􏰄'
+	
+	else
+      render :show
+    end
+
+
+
+
+end
+
+def cart_params
+    params.require(:cart).permit(line_items_attributes: [:id, :quantity])
+end
+
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,6 @@
 class Cart < ActiveRecord::Base
 	has_many :line_items , dependent: :destroy
+	accepts_nested_attributes_for :line_items
+	validates_associated :line_items
+
 end
