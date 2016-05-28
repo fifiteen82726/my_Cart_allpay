1f6a44bec39c0c90f1eaf5be79fab80e6fde1f6b
fifiteen82726 <fifiteen82726@gmail.com>
2016-05-29 00:01:38 +0800

    加上了上方的連結,改了product index的頁面

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -10,6 +10,7 @@ class LineItem < ActiveRecord::Base
 	def check_stock
 	  errors.add(:quantity, 'out of stock') if quantity > product.stock
 	end
+	
 	def set_unit_price
 	  self.unit_price = product.price
 	end
